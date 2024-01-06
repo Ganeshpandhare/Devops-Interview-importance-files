a2f82aa25fdc90862ee4a9405a48405ae32bcdfe
Ganeshpandhare <151309455+Ganeshpandhare@users.noreply.github.com>
2024-01-06 15:11:28 +0530

    jenkins-terraform-project-file

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-2" 
+}
+
+variable "key_name" {
+       description = "creating key" 
+       default     = "Ohio" 
+}
